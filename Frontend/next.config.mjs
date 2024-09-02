6ed05ce383ec7cffbd0ae6f6c45d3e654699e076
collegeparam <138299927+param2610-cloud@users.noreply.github.com>
2024-09-02 20:54:08 +0530

    ...

--- Frontend/next.config.mjs
+++ Frontend/next.config.mjs
@@ -1,4 +1,13 @@
+import path from 'path';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    // Add alias for @ symbol
+    config.resolve.alias['@'] = path.resolve('src');
+    
+    return config;
+  },
+};
 
 export default nextConfig;
