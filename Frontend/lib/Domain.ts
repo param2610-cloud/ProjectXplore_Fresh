12977a7aef85dd85a920c65a6e1880765b625acc
collegeparam <138299927+param2610-cloud@users.noreply.github.com>
2024-09-07 21:06:25 +0530

    domain

--- Frontend/lib/Domain.ts
+++ Frontend/lib/Domain.ts
@@ -1,4 +1,4 @@
-const Domain = "http://localhost:8080"
-const FrontendDomain = "http://localhost:3000"
+const Domain = "https://projectxplore-rust.vercel.app/"
+const FrontendDomain = "https://projectxplore.vercel.app/"
 const FirebaseUrl = "https://projectxplore-7c636-default-rtdb.firebaseio.com/notifications.json"
 export {Domain,FrontendDomain,FirebaseUrl}
