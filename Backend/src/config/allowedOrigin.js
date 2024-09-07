07fa6761b4fcb84a07b04121f7b037d37532e381
collegeparam <138299927+param2610-cloud@users.noreply.github.com>
2024-09-07 20:33:16 +0530

    origin

--- Backend/src/config/allowedOrigin.js
+++ Backend/src/config/allowedOrigin.js
@@ -2,5 +2,7 @@ export const allowedOrigins = [
     'https://enterprise-management-system.vercel.app',
     'http://localhost:3000',
     'http://localhost:5173',
-    'http://192.168.0.158:3000'
+    'http://192.168.0.158:3000',
+    "https://projectxplore.vercel.app/"
+
 ]
