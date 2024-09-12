a34d26e5527343d5599002bd238f0d9c9cae42f5
collegeparam <138299927+param2610-cloud@users.noreply.github.com>
2024-09-12 20:19:05 +0530

    Server is restored 3

--- Frontend/lib/Domain.ts
+++ Frontend/lib/Domain.ts
@@ -1,4 +1,5 @@
-const Domain = "https://projectxplore-rust.vercel.app"
+// const Domain = "https://projectxplore-rust.vercel.app"
+const Domain = "http://192.168.0.158:8080"
 const FrontendDomain = "https://projectxplore.vercel.app"
 const FirebaseUrl = "https://projectxplore-7c636-default-rtdb.firebaseio.com/notifications.json"
 export {Domain,FrontendDomain,FirebaseUrl}
