b0c79804cb391809b42fd1df23003cec61a33d92
collegeparam <138299927+param2610-cloud@users.noreply.github.com>
2024-09-12 20:19:05 +0530

    Server is restored 3

--- Backend/src/config/allowedOrigin.js
+++ Backend/src/config/allowedOrigin.js
@@ -1,6 +1,5 @@
 export const allowedOrigins = [
     "https://projectxplore.vercel.app",
-    'https://enterprise-management-system.vercel.app',
     'http://localhost:3000',
     'http://localhost:5173',
     'http://192.168.0.158:3000',
