945ca3347dcd2910ca45e3ca140f4056e96f854c
collegeparam <138299927+param2610-cloud@users.noreply.github.com>
2024-09-13 22:59:05 +0530

    Server is restored 4

--- Backend/src/config/allowedOrigin.js
+++ Backend/src/config/allowedOrigin.js
@@ -1,5 +1,6 @@
 export const allowedOrigins = [
     "https://projectxplore.vercel.app",
+    "https://projectxplore-rust.vercel.app",
     'http://localhost:3000',
     'http://localhost:5173',
     'http://192.168.0.158:3000',
