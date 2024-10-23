e22ccd0bdf956bd16d50000c90e5bc2e405d72a8
collegeparam <138299927+param2610-cloud@users.noreply.github.com>
2024-10-24 01:04:28 +0530

    landing page ready

--- Frontend/tailwind.config.ts
+++ Frontend/tailwind.config.ts
@@ -1,5 +1,8 @@
 import type { Config } from "tailwindcss";
-
+const svgToDataUri = require("mini-svg-data-uri");
+const {
+	default: flattenColorPalette,
+  } = require("tailwindcss/lib/util/flattenColorPalette");
 const config = {
     darkMode: ["class"],
     content: [
@@ -65,6 +68,14 @@ const config = {
     			sm: 'calc(var(--radius) - 4px)'
     		},
     		keyframes: {
+				meteor: {
+					"0%": { transform: "rotate(215deg) translateX(0)", opacity: "1" },
+					"70%": { opacity: "1" },
+					"100%": {
+					  transform: "rotate(215deg) translateX(-500px)",
+					  opacity: "0",
+					},
+				  },
     			'accordion-down': {
     				from: {
     					height: '0'
@@ -99,6 +110,7 @@ const config = {
     			},
     		},
     		animation: {
+				"meteor-effect": "meteor 5s linear infinite",
     			fadeIn: 'fadeIn 0.5s ease-in-out',
     			fadeOut: 'fadeOut 0.5s ease-in-out',
     			'accordion-down': 'accordion-down 0.2s ease-out',
@@ -106,7 +118,28 @@ const config = {
     		}
     	}
     },
-    plugins: [require("tailwindcss-animate")],
+    plugins: [require("tailwindcss-animate"),addVariablesForColors,function ({ matchUtilities, theme }: any) {
+		matchUtilities(
+		  {
+			"bg-dot-thick": (value: any) => ({
+			  backgroundImage: `url("${svgToDataUri(
+				`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="2.5"></circle></svg>`
+			  )}")`,
+			}),
+		  },
+		  { values: flattenColorPalette(theme("backgroundColor")), type: "color" }
+		);
+	  },],
 } satisfies Config;
 
 export default config;
+function addVariablesForColors({ addBase, theme }: any) {
+	let allColors = flattenColorPalette(theme("colors"));
+	let newVars = Object.fromEntries(
+	  Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+	);
+   
+	addBase({
+	  ":root": newVars,
+	});
+  }
