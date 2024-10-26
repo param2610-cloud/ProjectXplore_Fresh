ea3599c7637648f43c0c479fb0bb03c9fca5df97
collegeparam <138299927+param2610-cloud@users.noreply.github.com>
2024-10-26 21:50:46 +0530

    domain set to prod

--- Frontend/lib/Domain.ts
+++ Frontend/lib/Domain.ts
@@ -1,6 +1,6 @@
-// const Domain = "https://projectxplore-rust.vercel.app"
-const Domain = "http://192.168.0.158:8080"
-// const FrontendDomain = "https://projectxplore.vercel.app"
-const FrontendDomain = "http://192.168.0.158:3000"
+const Domain = "https://projectxplore-rust.vercel.app"
+// const Domain = "http://192.168.0.158:8080"
+const FrontendDomain = "https://projectxplore.vercel.app"
+// const FrontendDomain = "http://192.168.0.158:3000"
 const FirebaseUrl = "https://projectxplore-7c636-default-rtdb.firebaseio.com/notifications.json"
 export {Domain,FrontendDomain,FirebaseUrl}
