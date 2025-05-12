1d260e60986db13fd440f862e273926d5965bbe1
Parambrata Ghosh <gpampa138@gmail.com>
2025-05-12 19:52:04 +0530

    fix: update Domain and FrontendDomain URLs to production

--- Frontend/lib/Domain.ts
+++ Frontend/lib/Domain.ts
@@ -1,6 +1,6 @@
 // const Domain = "https://projectxplore-rust.vercel.app"
-const Domain = "http://localhost:8080"
+const Domain = "https://projectxplore-rust.vercel.app"
 // const FrontendDomain = "https://projectxplore.vercel.app"
-const FrontendDomain = "http://192.168.0.158:3000"
+const FrontendDomain = "https://projectxplore.vercel.app"
 const FirebaseUrl = "https://projectxplore-7c636-default-rtdb.firebaseio.com/notifications.json"
 export {Domain,FrontendDomain,FirebaseUrl}
